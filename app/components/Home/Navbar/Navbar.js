eb61a69e3d1d42ce619837981fb55a4432c3fbdd
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-17 00:58:21 +0300

    fixed some bugs and improved navbar

--- app/components/Home/Navbar/Navbar.js
+++ app/components/Home/Navbar/Navbar.js
@@ -6,16 +6,18 @@ export default function Navbar() {
   return (
     <div className="h-[12vh] z-[100] w-full top-0 sticky">
       <div
-        className="flex items-center h-full w-[80%] xl:w-[90%] mx-auto bg-[#1E1E1E] mt-5 rounded-xl
+        className="flex items-center h-full w-[95%] xl:w-[90%] mx-auto bg-[#1E1E1E] mt-5 rounded-xl
       "
       >
         {/* Logo Section  */}
         <div className="flex items-center justify-between align-middle w-[80%] h-[60%] mx-auto">
-          <h1 className={`${jost.className} text-white font-bold text-2xl`}>
+          <h1
+            className={`${jost.className} text-white font-bold text-2xl select-none`}
+          >
             BAYS
           </h1>
 
-          <div className="absolute left-1/2 -translate-x-[75%] flex space-x-6 text-white font-light">
+          <div className="hidden md:flex absolute left-1/2 -translate-x-[75%] space-x-6 text-white font-light">
             <a
               href=""
               className="hover:underline hover:underline-offset-4 decoration-[1px]"
@@ -35,7 +37,7 @@ export default function Navbar() {
               Pricing
             </a>
           </div>
-          <div className="ml-auto flex space-x-4">
+          <div className="ml-auto flex space-x-4 sm:w-auto justify-end pr-5">
             <a
               href="#_"
               className="relative block w-auto px-6 py-3 overflow-hidden text-base font-semibold text-center text-gray-800 rounded-lg bg-white"
