56e9596a63f17274fc5e0d4b1a0da5b6db01b288
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-17 00:01:02 +0300

    added logo of bays

--- app/components/Home/Navbar/Navbar.js
+++ app/components/Home/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 export default function Navbar() {
   return (
-    <div className="h-[12vh] z-[100] fixed w-full">
+    <div className="h-[12vh] z-[100] fixed w-full top-0 sticky">
       <div
-        className="flex items-center h-full w-[90%] mx-auto bg-red-300 mt-5 rounded-xl
+        className="flex items-center h-full w-[80%] xl:w-[90%] mx-auto bg-[#1E1E1E] mt-5 rounded-xl
       "
-      ></div>
+      >
+        <h1 className="text-white font-bold text-2xl ml-10">BAYS</h1>
+      </div>
     </div>
   );
 }
