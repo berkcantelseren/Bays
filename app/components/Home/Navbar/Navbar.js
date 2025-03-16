5a275490eb2a543679eb79cfb2342ccacbf0bea7
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-16 23:56:21 +0300

    removed globalcss and started layout for navbar

--- app/components/Home/Navbar/Navbar.js
+++ app/components/Home/Navbar/Navbar.js
@@ -1,3 +1,10 @@
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className="h-[12vh] z-[100] fixed w-full">
+      <div
+        className="flex items-center h-full w-[90%] mx-auto bg-red-300 mt-5 rounded-xl
+      "
+      ></div>
+    </div>
+  );
 }
