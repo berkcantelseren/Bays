93aa1d005afc801d27c76730e5a2f44ad749d368
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-18 00:18:58 +0300

    added jost font for bays logo

--- app/components/Home/Hero/Hero.js
+++ app/components/Home/Hero/Hero.js
@@ -1,10 +1,16 @@
+import { Jost } from "next/font/google";
+
+// Fonts
+const jost = Jost({ subsets: ["latin"], weight: "700" });
+
 export default function Hero() {
   return (
     <div className=" flex w-[70%] justify-center mx-auto mt-15 h-[65vh]">
       <div className="text-center">
         <h1 className="text-white text-4xl">Work Smarter,</h1>
         <span className="block ml-[-30px] text-white text-4xl">
-          Not Harder with <span className="">BAYS</span>
+          Not Harder with
+          <span className={`${jost.className} text-white font-bold`}>BAYS</span>
         </span>
         <p className="text-sm text-[#F6E9E9]/60 mt-5">
           Streamline Workflows, Save Time, and 2x your productivity with BAYS
