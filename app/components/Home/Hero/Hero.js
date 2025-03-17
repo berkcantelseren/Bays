0a6132b8f299c40c327b2c0f0c0232b7359e0691
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-18 00:29:17 +0300

    hero section text finished

--- app/components/Home/Hero/Hero.js
+++ app/components/Home/Hero/Hero.js
@@ -1,18 +1,28 @@
 import { Jost } from "next/font/google";
 
 // Fonts
-const jost = Jost({ subsets: ["latin"], weight: "700" });
+const jost = Jost({ subsets: ["latin"], weight: "800" });
+const jostMd = Jost({ subsets: ["latin"], weight: "500" });
+const jostSm = Jost({ subsets: ["latin"], weight: "300" });
 
 export default function Hero() {
   return (
     <div className=" flex w-[70%] justify-center mx-auto mt-15 h-[65vh]">
       <div className="text-center">
-        <h1 className="text-white text-4xl">Work Smarter,</h1>
-        <span className="block ml-[-30px] text-white text-4xl">
-          Not Harder with
+        <h1
+          className={`text-white text-5xl font-semibold mb-3 ${jostMd.className}`}
+        >
+          Work Smarter,
+        </h1>
+        <span
+          className={`block ml-[-30px] text-white text-5xl font-semibold ${jostMd.className}`}
+        >
+          Not Harder with{" "}
           <span className={`${jost.className} text-white font-bold`}>BAYS</span>
         </span>
-        <p className="text-sm text-[#F6E9E9]/60 mt-5">
+        <p
+          className={`text-[16px] text-[#F6E9E9]/60 mt-5 mr-7 ${jostSm.className}`}
+        >
           Streamline Workflows, Save Time, and 2x your productivity with BAYS
         </p>
       </div>
