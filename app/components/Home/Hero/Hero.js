7e1805065ca010c0e1e55194aa0574cf1371ece9
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-18 01:11:27 +0300

    hero section finished and 2 svg files added for hero section buttons.

--- app/components/Home/Hero/Hero.js
+++ app/components/Home/Hero/Hero.js
@@ -1,4 +1,6 @@
 import { Jost } from "next/font/google";
+import Image from "next/image";
+import { RxTriangleRight } from "react-icons/rx";
 
 // Fonts
 const jost = Jost({ subsets: ["latin"], weight: "800" });
@@ -7,24 +9,44 @@ const jostSm = Jost({ subsets: ["latin"], weight: "300" });
 
 export default function Hero() {
   return (
-    <div className=" flex w-[70%] justify-center mx-auto mt-15 h-[65vh]">
-      <div className="text-center">
+    <div className=" flex flex-col w-[65%] justify-center mx-auto mt-15 h-[150h]">
+      {/* Text Section  */}
+      <div className="text-center md:space-y-3">
         <h1
-          className={`text-white text-5xl font-semibold mb-3 ${jostMd.className}`}
+          className={`text-white text-3xl sm:text-4xl md:text-5xl font-semibold  ${jostMd.className}`}
         >
           Work Smarter,
         </h1>
         <span
-          className={`block ml-[-30px] text-white text-5xl font-semibold ${jostMd.className}`}
+          className={`block text-white text-3xl sm:text-4xl md:text-5xl font-semibold ${jostMd.className}`}
         >
           Not Harder with{" "}
           <span className={`${jost.className} text-white font-bold`}>BAYS</span>
         </span>
         <p
-          className={`text-[16px] text-[#F6E9E9]/60 mt-5 mr-7 ${jostSm.className}`}
+          className={` text-sm md:text-[16px] text-[#F6E9E9]/60 mt-5 ${jostSm.className}`}
         >
           Streamline Workflows, Save Time, and 2x your productivity with BAYS
         </p>
+        <div className="flex gap-5 justify-center mt-10">
+          <a
+            href="#_"
+            className="relative flex items-center justify-center w-auto min-w-[160px] px-6 py-3 overflow-hidden text-base font-semibold text-center text-white rounded-lg bg-[#1E1E1E] whitespace-nowrap"
+          >
+            Get Started for Free
+          </a>
+          <a
+            href="#_"
+            className="relative flex items-center justify-center gap-2 w-auto min-w-[165px] px-6 py-3 overflow-hidden text-base font-semibold text-center text-gray-800 rounded-lg bg-gray-50 whitespace-nowrap"
+          >
+            <Image src="/images/arrow.svg" width={20} height={20} /> Watch a
+            Demo
+          </a>
+        </div>
+      </div>
+      {/* Demo Example  */}
+      <div className="mt-15 hidden md:flex">
+        <Image src="/images/demo.svg" width={1056} height={578} />
       </div>
     </div>
   );
