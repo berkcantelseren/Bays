b0635ade45b1702072011515a34066fef471c921
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-17 22:28:55 +0300

    added mobile hamburger function and use client for navbar

--- app/components/Home/Navbar/Navbar.js
+++ app/components/Home/Navbar/Navbar.js
@@ -1,9 +1,16 @@
-import { Jost } from "next/font/google";
-import { HiBars3BottomRight, HiMiniBars3BottomRight } from "react-icons/hi2";
+"use client";
 
+import { useState } from "react";
+import { Jost } from "next/font/google";
+import { HiMiniBars3BottomRight } from "react-icons/hi2";
+import { CgClose } from "react-icons/cg";
+import Link from "next/link";
+
+// Fonts
 const jost = Jost({ subsets: ["latin"], weight: "700" });
 
 export default function Navbar() {
+  const [showNav, setShowNav] = useState(false);
   return (
     <div className="h-[12vh] z-[100] w-full top-0 sticky">
       <div
@@ -52,9 +59,42 @@ export default function Navbar() {
               Sign Up
             </a>
           </div>
-          <HiMiniBars3BottomRight className="w-8 h-8 text-white cursor-pointer lg:hidden " />
+          <HiMiniBars3BottomRight
+            className="w-8 h-8 text-white cursor-pointer lg:hidden"
+            onClick={() => setShowNav(true)}
+          />
         </div>
       </div>
+      {showNav && (
+        <div className="fixed inset-0 bg-[#1E1E1E]/60 z-[1002] flex items-center justify-center">
+          {/* Sidebar Menu */}
+          <div className="fixed top-0 left-0 h-full w-[80%] sm:w-[60%] bg-[#1E1E1E] p-6 text-white transform transition-all duration-500 delay-300">
+            {/* Close Button */}
+            <CgClose
+              className="absolute top-4 right-5 w-8 h-8 cursor-pointer"
+              onClick={() => setShowNav(false)}
+            />
+
+            <ul className="space-y-6 mt-10 text-lg">
+              <li>
+                <Link href="#" onClick={() => setShowNav(false)}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="#" onClick={() => setShowNav(false)}>
+                  Featuring
+                </Link>
+              </li>
+              <li>
+                <Link href="#" onClick={() => setShowNav(false)}>
+                  Pricing
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
