f0715a7d9e2b1f04c01572af4cbc12411a977e10
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-18 00:16:22 +0300

    started hero layout

--- app/components/Home/Hero/Hero.js
+++ app/components/Home/Hero/Hero.js
@@ -1,8 +1,15 @@
 export default function Hero() {
   return (
-    <div>
-      hero section
-      <div>test</div>
+    <div className=" flex w-[70%] justify-center mx-auto mt-15 h-[65vh]">
+      <div className="text-center">
+        <h1 className="text-white text-4xl">Work Smarter,</h1>
+        <span className="block ml-[-30px] text-white text-4xl">
+          Not Harder with <span className="">BAYS</span>
+        </span>
+        <p className="text-sm text-[#F6E9E9]/60 mt-5">
+          Streamline Workflows, Save Time, and 2x your productivity with BAYS
+        </p>
+      </div>
     </div>
   );
 }
