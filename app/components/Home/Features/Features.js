7f9934d40bf8add6d8b6ecc1eca7a89de1bcc4e6
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-19 23:16:35 +0300

    finished features section

--- app/components/Home/Features/Features.js
+++ app/components/Home/Features/Features.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import BoxText from "../../Helper/BoxText";
 import { Jost } from "next/font/google";
 
@@ -7,7 +8,7 @@ const jostMd = Jost({ subsets: ["latin"], weight: "500" });
 
 export default function Features() {
   return (
-    <div className="flex flex-col w-[60%] justify-center items-center mx-auto mt-15 h-[150h] bg-rose-200">
+    <div className="flex flex-col w-[60%] justify-center items-center mx-auto mt-15 h-[150h]">
       <div className="text-center md:space-y-3">
         <div className="flex text-center mx-auto">
           <BoxText>Features</BoxText>
@@ -23,6 +24,10 @@ export default function Features() {
       >
         that help your team achieve more
       </span>
+      {/* Features Example  */}
+      <div className="mt-15 hidden md:flex justify-center items-center border border-[#1E1E1E] p-5 rounded-xl">
+        <Image src="/images/features.svg" width={1056} height={578} />
+      </div>
     </div>
   );
 }
