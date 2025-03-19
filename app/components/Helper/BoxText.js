1bf4b5cc280dedf3c95b03ca5aec19e17a7d0e4b
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-19 23:10:02 +0300

    features text section finished

--- app/components/Helper/BoxText.js
+++ app/components/Helper/BoxText.js
@@ -3,7 +3,7 @@ import React from "react";
 const BoxText = ({ children }) => {
   return (
     <div>
-      <h1 className="w-fit py-1 text-[#1C54E3] font-bold">{children}</h1>
+      <h1 className="w-fit py-1 text-[#1C54E3] text-md mb-5">{children}</h1>
     </div>
   );
 };
--- app/components/Home/Features/Features.js
+++ app/components/Home/Features/Features.js
@@ -1,7 +1,28 @@
+import BoxText from "../../Helper/BoxText";
+import { Jost } from "next/font/google";
+
+// Fonts
+
+const jostMd = Jost({ subsets: ["latin"], weight: "500" });
+
 export default function Features() {
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-col w-[60%] justify-center items-center mx-auto mt-15 h-[150h]">
+      <div className="text-center md:space-y-3">
+        <div className="flex text-center mx-auto">
+          <BoxText>Features</BoxText>
+        </div>
+      </div>
+      <h1
+        className={`text-white text-3xl sm:text-4xl md:text-[40px] font-semibold  ${jostMd.className}`}
+      >
+        Discover features
+      </h1>
+      <span
+        className={`block text-white text-3xl sm:text-4xl md:text-[40px] font-semibold ${jostMd.className}`}
+      >
+        that help your team achieve more
+      </span>
     </div>
   );
 }
