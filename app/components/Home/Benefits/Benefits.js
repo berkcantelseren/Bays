7538da848209e37100d4d64824c610569debe09d
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-20 06:16:53 +0300

    add custom gradient to benefits section and created benefits section from scratch instead of using svg for whole grid

--- app/components/Home/Benefits/Benefits.js
+++ app/components/Home/Benefits/Benefits.js
@@ -9,7 +9,7 @@ const jostSm = Jost({ subsets: ["latin"], weight: "300" });
 
 export default function Benefits() {
   return (
-    <div className="flex flex-col w-[60%] justify-center items-center mx-auto mt-15 h-[150h]">
+    <div className="flex flex-col w-[90%] justify-center items-center mx-auto mt-15 h-[150h]">
       <div className="text-center md:space-y-3">
         <div className="flex text-center mx-auto">
           <BoxText>Benefits Section</BoxText>
@@ -25,26 +25,72 @@ export default function Benefits() {
       >
         Transformed the way we work
       </p>
-      {/* Features Example  */}
-      <div className="grid gap-20 grid-cols-3 grid-rows-2 mt-15 justify-center items-center border border-[#1E1E1E] p-5 rounded-xl">
-        <div className="col-span-1 row-span-2 bg-rose-200">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, quis.
-        </div>
-        <div className="col-span-1 row-span-2 bg-blue-300">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci,
-          vitae.
-        </div>
-        <div className="col-span-1 row-span-2 bg-green-300">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci,
-          vitae.
+      {/* 3 Grid Items */}
+      <div className="grid grid-cols-3 gap-10 mt-10 p-5 rounded-xl">
+        {/* Grid Item 1 */}
+        <div className="flex flex-col items-center justify-center w-[408px] h-[434px] bg-[#1E1E1E] rounded-3xl">
+          {/* Inner Grid: Icon */}
+          <div className="flex justify-center items-center p-16 pr-20 pl-20 gradient-custom rounded-3xl border border-[#3D3D3F]">
+            <Image
+              src="/images/reminder.svg"
+              alt="reminder logo"
+              width={140}
+              height={140}
+              className="select-none"
+            />
+          </div>
+          {/* Inner Grid: Text */}
+          <h1 className="mt-4 text-white text-center mb-5 font-semibold text-2xl">
+            Time Efficient
+          </h1>
+          <p className="text-center text-[16px] text-[#ADADAD]">
+            Save up to 20 hours per week with{" "}
+            <span className="block">automation</span>
+          </p>
         </div>
 
-        {/* <Image
-          src="/images/benefits.svg"
-          alt="benefits image"
-          width={1056}
-          height={578}
-        /> */}
+        {/* Grid Item 2 */}
+        <div className="flex flex-col items-center justify-center w-[408px] h-[434px] bg-[#1E1E1E] rounded-3xl">
+          {/* Inner Grid: Icon */}
+          <div className="flex justify-center items-center p-16 pr-20 pl-20 gradient-custom rounded-3xl border border-[#3D3D3F]">
+            <Image
+              src="/images/repeat.svg"
+              alt="reminder logo"
+              width={140}
+              height={140}
+              className="select-none"
+            />
+          </div>
+          {/* Inner Grid: Text */}
+          <h1 className="mt-4 text-white text-center mb-5 font-semibold text-2xl">
+            All-in-One
+          </h1>
+          <p className="text-center text-[16px] text-[#ADADAD]">
+            Centralized platform for all your tools and{" "}
+            <span className="block">data.</span>
+          </p>
+        </div>
+        {/* Grid Item 3 */}
+        <div className="flex flex-col items-center justify-center w-[408px] h-[434px] bg-[#1E1E1E] rounded-3xl">
+          {/* Inner Grid: Icon */}
+          <div className="flex justify-center items-center p-16 pr-20 pl-20 gradient-custom rounded-3xl border border-[#3D3D3F]">
+            <Image
+              src="/images/create.svg"
+              alt="reminder logo"
+              width={140}
+              height={140}
+              className="select-none"
+            />
+          </div>
+          {/* Inner Grid: Text */}
+          <h1 className="mt-4 text-white text-center mb-5 font-semibold text-2xl">
+            Easy Use
+          </h1>
+          <p className="text-center text-[16px] text-[#ADADAD]">
+            User-friendly interface for quick{" "}
+            <span className="block">onboarding</span>
+          </p>
+        </div>
       </div>
     </div>
   );
