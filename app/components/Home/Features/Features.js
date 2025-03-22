c1eb59181356c003f9f83d8692a75a38c229a26c
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-22 08:56:04 +0300

    created features section layout and add selection to whole website

--- app/components/Home/Features/Features.js
+++ app/components/Home/Features/Features.js
@@ -8,7 +8,7 @@ const jostMd = Jost({ subsets: ["latin"], weight: "500" });
 
 export default function Features() {
   return (
-    <div className="flex flex-col w-[60%] justify-center items-center mx-auto mt-15 h-[150h]">
+    <div className="flex flex-col w-[90%] justify-center items-center mx-auto mt-15 h-[150h]">
       <div className="text-center md:space-y-3">
         <div className="flex text-center mx-auto">
           <BoxText>Features</BoxText>
@@ -25,14 +25,36 @@ export default function Features() {
         that help your team achieve more
       </span>
       {/* Features Example  */}
-      <div className="mt-15 hidden md:flex justify-center items-center border border-[#1E1E1E] p-5 rounded-xl">
+      <div className="border">
+        <div className="grid grid-cols-5 grid-rows-5 gap-3 mt-15">
+          <div className="w-[230] h-[50] rounded-xl text-[#ADADAD] bg-[#1E1E1E] flex items-center justify-center">
+            Task Management
+          </div>
+          <div className="w-[230] h-[50] text-[#ADADAD] bg-[#1E1E1E] flex items-center justify-center rounded-xl">
+            Collaboration Tools
+          </div>
+          <div className="w-[230] h-[50] text-[#ADADAD] bg-[#1E1E1E] flex items-center justify-center rounded-xl">
+            Analytics and Reporting
+          </div>
+          <div className="w-[230] h-[50] text-[#ADADAD] bg-[#1E1E1E] flex items-center justify-center rounded-xl">
+            Custom Integrations
+          </div>
+          <div className="w-[230] h-[50] text-[#ADADAD] bg-[#1E1E1E] flex items-center justify-center rounded-xl">
+            Easy to Use
+          </div>
+          <div className="col-span-3 row-span-7 bg-[#1E1E1E] rounded-4xl w-[700] h-[385]">
+            s
+          </div>
+        </div>
+      </div>
+      {/* <div className="mt-15 hidden md:flex justify-center items-center border border-[#1E1E1E] p-5 rounded-xl">
         <Image
           src="/images/features.svg"
           alt="features image"
-          width={1056}
-          height={578}
+          width={1272}
+          height={558}
         />
-      </div>
+      </div> */}
     </div>
   );
 }
