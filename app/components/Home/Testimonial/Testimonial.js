1a000a32be2a005d5b3fab12e3b87464d2c572fa
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-03 23:08:14 +0300

    created testimonial section + file

---
+++ app/components/Home/Testimonial/Testimonial.js
@@ -0,0 +1,11 @@
+export default function Testimonial() {
+  return (
+    <div className="flex flex-col w-[90%] justify-center items-center mx-auto mt-30 h-full">
+      <div className="text-center md:space-y-3">
+        <div className="flex text-center mx-auto">
+          <BoxText>Testimonial</BoxText>
+        </div>
+      </div>
+    </div>
+  );
+}
