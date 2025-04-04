91e82b13eb459bc6eb9d8f838514aec6cc0bc472
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-04 22:50:48 +0300

    added testimonial heading and text

--- app/components/Home/Testimonial/Testimonial.js
+++ app/components/Home/Testimonial/Testimonial.js
@@ -1,4 +1,10 @@
 import BoxText from "../../Helper/BoxText";
+import { Jost } from "next/font/google";
+
+// Fonts
+
+const jostMd = Jost({ subsets: ["latin"], weight: "500" });
+const jostSm = Jost({ subsets: ["latin"], weight: "300" });
 
 export default function Testimonial() {
   return (
@@ -8,6 +14,29 @@ export default function Testimonial() {
           <BoxText>Testimonial</BoxText>
         </div>
       </div>
+      <h1
+        className={`text-white text-3xl sm:text-4xl md:text-[40px] font-semibold  ${jostMd.className}`}
+      >
+        Trusted by Many
+      </h1>
+      <p
+        className={` text-sm md:text-[16px] text-[#F6E9E9]/60 mt-5 ${jostSm.className}`}
+      >
+        Turn your visitors into loyal clients with heartfelt testimonials.
+      </p>
+      <p
+        className={` text-sm md:text-[16px] text-[#F6E9E9]/60 ${jostSm.className}`}
+      >
+        Use{" "}
+        <a
+          href="https://www.feedspace.io/"
+          target="blank"
+          className="text-[#1C54E3] "
+        >
+          Feedspace
+        </a>{" "}
+        to create a wall of love or testimonial widget for your website
+      </p>
     </div>
   );
 }
