397690f00cf0b244d2114e8f84ce821108c1b0a2
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-12 23:03:37 +0300

    used helper BoxText for how it works section

--- app/components/Home/HowItWorks/HowItWorks.js
+++ app/components/Home/HowItWorks/HowItWorks.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import BoxText from "../../Helper/BoxText";
 
 export default function HowItWorks() {
   return (
     <div className="flex bg-[#1E1E1E] w-[90%] justify-center items-center mx-auto h-[542px]">
       <div className="grid grid-cols-2 gap-50">
-        <div>1</div>
+        <div className="w-[460] h-[360] flex justify-start bg-rose-300">
+          <BoxText>How It Works Section</BoxText>
+        </div>
         <div className="bg-[#1F1F1F] gradient-custom w-[516] h-[398] flex justify-center items-center align-center">
           <Image
             src="images/step.svg"
