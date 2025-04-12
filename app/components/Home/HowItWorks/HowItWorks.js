1791af94c95cadb4a772d932ef78c9fcd50be4d8
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-12 23:01:02 +0300

    finished second grid for how it works

--- app/components/Home/HowItWorks/HowItWorks.js
+++ app/components/Home/HowItWorks/HowItWorks.js
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 export default function HowItWorks() {
   return (
     <div className="flex bg-[#1E1E1E] w-[90%] justify-center items-center mx-auto h-[542px]">
       <div className="grid grid-cols-2 gap-50">
         <div>1</div>
-        <div>2</div>
+        <div className="bg-[#1F1F1F] gradient-custom w-[516] h-[398] flex justify-center items-center align-center">
+          <Image
+            src="images/step.svg"
+            alt="step icon"
+            width={350}
+            height={300}
+            className="text-gray-500 font-bold"
+          />
+        </div>
       </div>
     </div>
   );
