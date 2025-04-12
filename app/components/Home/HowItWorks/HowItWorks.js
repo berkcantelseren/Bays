74ddeecd1ac17f2ff18496d8c2a3c5ff5b8e43e5
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-12 23:33:16 +0300

    finished how it works section and made it ready for responsive

--- app/components/Home/HowItWorks/HowItWorks.js
+++ app/components/Home/HowItWorks/HowItWorks.js
@@ -1,21 +1,29 @@
 import Image from "next/image";
 import BoxText from "../../Helper/BoxText";
+import { Jost } from "next/font/google";
+
+const jostMd = Jost({ subsets: ["latin"], weight: "500" });
+const jostSm = Jost({ subsets: ["latin"], weight: "300" });
 
 export default function HowItWorks() {
   return (
-    <div className="flex bg-[#1E1E1E] w-[90%] justify-center items-center mx-auto h-[542px]">
-      <div className="grid grid-cols-2 gap-40">
+    <div className="flex bg-[#1E1E1E] w-[90%] justify-center items-center mx-auto h-full md:h-[542px] rounded-2xl border border-[#3D3D3F] mt-90 md:mt-0">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-40">
         {/* Grid Item 1 */}
         <div className="w-[460] h-[360] flex flex-col justify-start">
           <BoxText>How It Works Section</BoxText>
           <div className="">
-            <h1 className="font-semibold text-[44px] text-white text-start">
+            <h1
+              className={`font-semibold text-[44px] text-white text-start  ${jostMd.className}`}
+            >
               Simple, Yet Powerful
             </h1>
           </div>
           {/* List items  */}
           <div>
-            <ol className="list-decimal pl-5 space-y-2 mt-10 text-[16px] text-[#ADADAD]">
+            <ol
+              className={`list-decimal pl-5 space-y-2 mt-10 text-[16px] text-[#ADADAD] ${jostMd.className}`}
+            >
               <li className="font-semibold mb-3">
                 Sign Up:{" "}
                 <span className="font-normal">
@@ -38,14 +46,19 @@ export default function HowItWorks() {
             </ol>
           </div>
           {/* Button  */}
+          <div>
+            <button className="bg-[#F6E9E9] w-[145] h-[47] font-semibold text-[16px] rounded-xl mt-7 cursor-pointer">
+              Get Started
+            </button>
+          </div>
         </div>
         {/* Grid Item 2 */}
-        <div className="bg-[#1F1F1F] gradient-custom w-[516] h-[398] flex justify-center items-center align-center">
+        <div className="bg-[#1F1F1F] gradient-custom w-[516] h-[398] flex justify-center items-center align-center rounded-2xl border border-[#3D3D3F]">
           <Image
             src="images/step.svg"
             alt="step icon"
-            width={350}
-            height={300}
+            width={450}
+            height={400}
             className="text-gray-500 font-bold"
           />
         </div>
