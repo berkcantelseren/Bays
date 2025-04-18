a323168a411ebd12c71b2247837ddce4ded30f19
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-18 23:12:46 +0300

    finished text section for faq and imported fonts

--- app/components/Home/Faq/Faq.js
+++ app/components/Home/Faq/Faq.js
@@ -1,7 +1,21 @@
+import BoxText from "../../Helper/BoxText";
+import { Jost } from "next/font/google";
+
+const jostMd = Jost({ subsets: ["latin"], weight: "500" });
+
 export default function Faq() {
   return (
-    <div className="bg-rose-300 flex flex-col w-[90%] justify-center items-center mx-auto mt-40 h-full">
-      <div>faq test</div>
+    <div className="flex flex-col w-[90%] justify-center items-center mx-auto mt-40 h-full">
+      <div className="text-center md:space-y-3">
+        <div className="flex text-center mx-auto">
+          <BoxText>FAQ</BoxText>
+        </div>
+      </div>
+      <h1
+        className={`text-white text-3xl sm:text-4xl md:text-[55px] font-semibold  ${jostMd.className}`}
+      >
+        Simple and Transparent Pricing
+      </h1>
     </div>
   );
 }
