cc02d4df7c037410f1bb6a754fcb2290d9179a3f
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-22 22:55:14 +0300

    created all grids for footer and fixed bugs

--- app/components/Home/Footer/Footer.js
+++ app/components/Home/Footer/Footer.js
@@ -1,7 +1,20 @@
 export default function Footer() {
   return (
-    <div className="h-[280px] w-[95%] xl:w-[90%] mx-auto items-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 bg-[#1E1E1E] border border-[#3D3D3F] mt-5 rounded-3xl pt-16 pb-16">
-      {/* 2st Section Footer  */}
+    <div className="h-auto w-[95%] xl:w-[90%] mx-auto items-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-5 bg-[#1E1E1E] border border-[#3D3D3F] mt-30 rounded-3xl pt-16 pb-16">
+      {/* Section 1*/}
+      <div className="space-y-3">
+        <h1>BAYS</h1>
+      </div>
+      {/* Section 2*/}
+      <div className="space-y-3 inline-block">
+        <h1 className="text-[16px] font-semibold text-[#1C54E3] mb-10">
+          Company
+        </h1>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">About Us</p>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Careers</p>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Blog</p>
+      </div>
+      {/* Section 3 */}
       <div className="space-y-3">
         <h1 className="text-[16px] font-semibold text-[#1C54E3] mb-10">
           Company
@@ -10,7 +23,16 @@ export default function Footer() {
         <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Careers</p>
         <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Blog</p>
       </div>
-      {/* 3rd Section Footer  */}
+      {/* Section 4 */}
+      <div className="space-y-3">
+        <h1 className="text-[16px] font-semibold text-[#1C54E3] mb-10">
+          Company
+        </h1>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">About Us</p>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Careers</p>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Blog</p>
+      </div>
+      {/* Section 5 */}
       <div className="space-y-3">
         <h1 className="text-[16px] font-semibold text-[#1C54E3] mb-10">
           Company
