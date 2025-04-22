9adb5a3c8033bb763fb17b54cb2469101ad1a331
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-22 22:41:40 +0300

    created grid for footer

--- app/components/Home/Footer/Footer.js
+++ app/components/Home/Footer/Footer.js
@@ -1,7 +1,24 @@
 export default function Footer() {
   return (
-    <div className="flex items-center h-[280px] w-[95%] xl:w-[90%] mx-auto bg-[#1E1E1E] border border-[#3D3D3F] mt-5 rounded-3xl">
-      <div>test</div>
+    <div className="h-[280px] w-[95%] xl:w-[90%] mx-auto items-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 bg-[#1E1E1E] border border-[#3D3D3F] mt-5 rounded-3xl pt-16 pb-16">
+      {/* 2st Section Footer  */}
+      <div className="space-y-3">
+        <h1 className="text-[16px] font-semibold text-[#1C54E3] mb-10">
+          Company
+        </h1>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">About Us</p>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Careers</p>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Blog</p>
+      </div>
+      {/* 3rd Section Footer  */}
+      <div className="space-y-3">
+        <h1 className="text-[16px] font-semibold text-[#1C54E3] mb-10">
+          Company
+        </h1>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">About Us</p>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Careers</p>
+        <p className="text-[#FFFFFF] text-[16px] cursor-pointer">Blog</p>
+      </div>
     </div>
   );
 }
