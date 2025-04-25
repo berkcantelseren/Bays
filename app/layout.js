2bc7e7184a68006698a28535f68e81b5ed8ced1e
Berkcan Telseren <berkcantelseren@gmail.com>
2025-04-25 23:24:50 +0300

    fixed name of the website and finished ready for publish.

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Bays",
+  title: "BAYS",
   description: "Bays collects testimonials in video, audio, or text formats.",
 };
 
